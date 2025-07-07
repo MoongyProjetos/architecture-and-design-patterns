71dd0cd678cfdf38f1609f3da76a1e0b596495bf
Jonatas Afonso <jonatas_afonso@hotmail.com>
2025-07-07 21:18:09 +0100

    fix: Update Event Hub connection string and name placeholders for clarity

--- src/Seguradora/12.SeguradoraEventDriven/PythonSample/Producer.py
+++ src/Seguradora/12.SeguradoraEventDriven/PythonSample/Producer.py
@@ -3,7 +3,8 @@ import asyncio
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 
-# Replace with your Event Hub connection string and event hub name.
+EVENT_HUB_CONNECTION_STR = "...your_event_hub_connection_string_here..."
+EVENT_HUB_NAME = "...your_event_hub_name_here..."
 
 async def run():
     # Create a producer client to send messages to the event hub.
